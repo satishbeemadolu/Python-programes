5551487d84eb53488793d49400dec972c86bb1bc
satishbeemadolu <sathish.bmdl@gmail.com>
2024-08-06 14:10:37 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+"""1.	Create a class named ‘Student’ with a string variable ‘name’ and an integer value ‘roll_no’.
+Assign the value of roll_no as ‘2’ and that of name as “John” by creating an object of the class Student.
+"""
+class Student:
+    def std(self,name,rollno):
+         print("student name:",name,"\nRoll_no:",rollno)
+        
+obj=Student()
+obj.std("john",2)
+
